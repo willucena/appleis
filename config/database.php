93760acdff23ca8b34a8a919d81fc5e7ff2a7bf9
Wilson Lucena <wilson@agencissisters.com.br>
2020-12-07 21:06:37 -0300

    database configurado

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '138.68.250.167'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'appleis'),
             'username' => env('DB_USERNAME', 'root'),
