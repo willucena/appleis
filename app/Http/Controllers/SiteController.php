44e2521ac1bcaada8a288dac2363b10ebea2c8b3
Wilson Lucena <wilson@agencissisters.com.br>
2020-12-08 22:35:06 -0300

    Cadastros de categorias e subCategorias

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -10,12 +10,13 @@ class SiteController extends Controller
     public function index(Request $request)
     {
         $perguntas = Pergunta::whereNull('parent_id')->get();
-        return view('index', compact('perguntas'));
+        $subCategorias = Pergunta::whereNotNull('parent_id')->get();
+        return view('index', compact('perguntas', 'subCategorias'));
     }
 
     public function respostas($id)
     {
-        $dados= Pergunta::where('parent_id', $id)->get();
+        $dados= Pergunta::where('id', $id)->get();
         return view('respostas', compact('dados'));
     }
 }
