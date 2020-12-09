43f2f1655ddb48b5333358f78defe8ef3fce4110
Wilson Lucena <wilson@agencissisters.com.br>
2020-12-09 00:03:21 -0300

    Desenvolvimeno finalizado

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -27,7 +27,6 @@
 <nav class="navbar navbar-light bg-light static-top">
     <div class="container">
         <a class="navbar-brand" href="/">{{ setting('site.title') }}</a>
-{{--        <a class="btn btn-primary" href="#">Todas as leis</a>--}}
     </div>
 </nav>
 
