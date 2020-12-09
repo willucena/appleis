d56dcc330a6c0b7d7907c5ca26c04947060499cf
Wilson Lucena <wilson@agencissisters.com.br>
2020-12-08 22:27:39 -0300

    Melhorias na pagina

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,15 +11,13 @@
     <title>SISLEI - Pesquisa de Leis Ambientais</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/simple-line-icons/css/simple-line-icons.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
     <!-- Custom styles for this template -->
-    <link href="css/landing-page.min.css" rel="stylesheet">
+    <link href="{{ asset('css/landing-page.min.css') }}" rel="stylesheet">
 
 </head>
 
@@ -28,8 +26,8 @@
 <!-- Navigation -->
 <nav class="navbar navbar-light bg-light static-top">
     <div class="container">
-        <a class="navbar-brand" href="#">{{ setting('site.title') }}</a>
-        <a class="btn btn-primary" href="#">Todas as leis</a>
+        <a class="navbar-brand" href="/">{{ setting('site.title') }}</a>
+{{--        <a class="btn btn-primary" href="#">Todas as leis</a>--}}
     </div>
 </nav>
 
@@ -46,8 +44,8 @@
 </footer>
 
 <!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
 </body>
 
